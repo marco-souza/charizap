a765f06077b73f9dbb08d378258c5110daa12cae
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-06 16:59:09 -0300

    ENH: add placeholder pages

---
+++ src/app/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Button from 'app/components/core/Button'
+
+function About (props) {
+  return (
+    <div>
+      <Button>
+        Login
+      </Button>
+    </div>
+  )
+}
+
+export default About
