927ba71bb5306dcf2fe674cdd5cc5863713ce032
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-08 14:28:52 -0300

    ENH: add grid to Login page

--- src/app/pages/Login/index.js
+++ src/app/pages/Login/index.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+// import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
 
+import LoginCover from 'app/components/LoginCover'
 import Button from 'app/components/core/Button'
+import Container from 'app/components/core/Container'
 
 import useAuth from 'app/redux/auth'
 
 const Login = ({ login }) => (
   <div>
-    <Button onClick={() => login('Loks')}>
-      Login
-    </Button>
+    <Container>
+      <Grid container>
+        <Grid item sm={6} xs={12}>
+          <LoginCover />
+        </Grid>
+
+        <Grid item sm={6} xs={12}>
+          {/* <Menu></Menu> */}
+          {/* <LoginForm></LoginForm> */}
+          <Button onClick={() => login('Loks')}>
+            Login
+          </Button>
+        </Grid>
+      </Grid>
+
+    </Container>
   </div>
 )
 
