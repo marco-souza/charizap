dfa351371ec6a1295ceea05547596c7f6f70210a
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-09 12:55:59 -0300

    FIX: update Contained bbutton styles

--- src/app/components/core/Button/ContainedButton.js
+++ src/app/components/core/Button/ContainedButton.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
-import { getPropsColor, getPropsBackground } from 'app/helpers/theme'
+import { getPropsBackground } from 'app/helpers/theme'
 
 import BaseButton from './BaseButton'
 
 const Button = styled(BaseButton)`
   /* Adapt the colors based on primary prop */
-  background: ${getPropsColor()};
+  background: ${getPropsBackground('primary')};
   color: ${getPropsBackground()};
 `
 
