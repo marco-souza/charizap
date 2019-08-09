302a39ddaceed82dc06b208b14eda78643dd2c3a
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-09 16:32:13 -0300

    ENH: fix text type problem

--- src/app/components/core/Text/component.js
+++ src/app/components/core/Text/component.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
 
 const Text = ({ className, children, variant }) => (
-  <Typography className={className} variant={variant}>
+  <Typography className={className} variant={variant !== 'span' ? variant : null}>
     {children}
   </Typography>
 )
 
 Text.propTypes = {
-  variant: PropTypes.string.isRequired,
+  variant: PropTypes.string,
   children: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
 }
