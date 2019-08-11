49b1d0893b898cbdfec8dbdc53a81b995bd8e939
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-11 00:23:04 -0300

    ENH: generalize Cover

--- src/app/components/LoginCover/index.js
+++ src/app/components/LoginCover/index.js
@@ -1,37 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withBreakpoints } from 'react-breakpoints'
 
 import Cover from 'app/components/core/Cover'
 
 import loginStarship from 'assets/images/login_starship.gif'
 
-const LoginCover = ({
-  className,
-  breakpoints,
-  currentBreakpoint,
-}) => (
+const LoginCover = ({ className }) => (
   <div className={className}>
-    {breakpoints[currentBreakpoint] > breakpoints.mobile
-      ? (
-        <Cover
-          background='primary'
-          logo='white'
-          message='Make an impact on the world'
-          className={className}
-          tittleUp
-        >
-          <img src={loginStarship} alt='' className='starship' />
-        </Cover>
-      )
-      : null
-    }
+    <Cover
+      background='primary'
+      logo='white'
+      message='Make an impact on the world'
+      className={className}
+      tittleUp
+    >
+      <img src={loginStarship} alt='' className='starship' />
+    </Cover>
   </div>
 )
+
 LoginCover.propTypes = {
   className: PropTypes.string,
-  breakpoints: PropTypes.object,
-  currentBreakpoint: PropTypes.string,
 }
 
-export default withBreakpoints(LoginCover)
+export default LoginCover
--- src/app/components/SignupCover/index.js
+++ src/app/components/SignupCover/index.js
@@ -1,38 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withBreakpoints } from 'react-breakpoints'
 
 import Cover from 'app/components/core/Cover'
 
 import Switch from './Switch'
 
-const SignupCover = ({
-  className,
-  breakpoints,
-  currentBreakpoint,
-}) => (
+const SignupCover = ({ className }) => (
   <div className={className}>
-    {breakpoints[currentBreakpoint] > breakpoints.mobile
-      ? (
-        <Cover
-          message='Focus on what really matters and leave it to us'
-          className={className}
-          logo='colored'
-          padding='40px'
-          renderBelow
-          moreSpace
-        >
-          <Switch />
-        </Cover>)
-      : null
-    }
+    <Cover
+      message='Focus on what really matters and leave it to us'
+      className={className}
+      logo='colored'
+      padding='40px'
+      renderBelow
+      moreSpace
+    >
+      <Switch />
+    </Cover>
   </div>
 )
 
 SignupCover.propTypes = {
   className: PropTypes.string,
-  breakpoints: PropTypes.object,
-  currentBreakpoint: PropTypes.string,
 }
 
-export default withBreakpoints(SignupCover)
+export default SignupCover
