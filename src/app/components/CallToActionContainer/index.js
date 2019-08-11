0f438a673684be48c8e89de6bda859191d165612
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-11 13:39:31 -0300

    REF: remove call to action

--- src/app/components/CallToActionContainer/index.js
+++
@@ -1,6 +0,0 @@
-import useAuth from 'app/redux/auth'
-import Component from './component'
-
-let ComposedComponent = useAuth(Component)
-
-export default ComposedComponent
--- src/app/pages/Dashboard/index.js
+++ src/app/pages/Dashboard/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 
-import CallToActionContainer from 'app/components/CallToActionContainer'
 import DashboardDeployments from 'app/components/DashboardDeployments'
 import Separator from 'app/components/core/Separator'
 
 const Dashboard = (props) => {
   return (
     <div>
-      Dashboard
-      <CallToActionContainer />
-
       <Separator />
 
       <DashboardDeployments />
