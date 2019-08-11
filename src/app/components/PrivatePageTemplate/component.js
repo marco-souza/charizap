4ea8bc583fbc7bc8b306458cf39cdc3d637354f6
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-10 22:07:45 -0300

    ENH: change spacing in private page template

--- src/app/components/PrivatePageTemplate/component.js
+++ src/app/components/PrivatePageTemplate/component.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
 
 import Menu from 'app/components/core/Menu'
 import Card from 'app/components/core/Card'
@@ -11,16 +10,14 @@ import Logo from 'app/components/core/Logo'
 const PrivatePageTemplate = ({ logout, children, className }) => (
   <div className={className}>
     <Container>
-      <Grid container>
-        <Menu
-          leftSide={<Logo />}
-          rightSide={<button onClick={logout}>logout</button>}
-        />
+      <Menu
+        leftSide={<Logo />}
+        rightSide={<button onClick={logout}>logout</button>}
+      />
 
-        <Card className='container'>
-          {children}
-        </Card>
-      </Grid>
+      <Card className='container'>
+        {children}
+      </Card>
     </Container>
   </div>
 )
