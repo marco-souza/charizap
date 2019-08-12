4dfaab3d6031604c7e5748f5f86a069df1ff4951
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-11 21:04:09 -0300

    FEA: add project details

---
+++ src/app/components/DashboardHeader/component.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
+
+import ProjectDetails from './ProjectDetails'
+
+const DashboardHeader = ({ className }) => (
+  <Grid container>
+    <Grid item md={6} xs={12}>
+      <ProjectDetails />
+    </Grid>
+    <Grid item md={4} xs={12}>
+      Chart
+    </Grid>
+    <Grid item md={2} xs={12}>
+      Actions
+    </Grid>
+
+  </Grid>
+)
+
+DashboardHeader.propTypes = {
+  className: PropTypes.string.isRequired,
+}
+
+export default DashboardHeader
