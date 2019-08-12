f3d699f28004598073fe8d0fdef0b7deb654326d
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-11 21:32:23 -0300

    FEA: add icon button

---
+++ src/app/components/core/Button/IconButton.js
@@ -0,0 +1,27 @@
+
+import styled from 'styled-components'
+
+import { getPropsColor, getPropsBackground } from 'app/helpers/theme'
+
+import BaseButton from './BaseButton'
+
+const Button = styled(BaseButton)`
+  /* Adapt the colors based on primary prop */
+  padding: 5px 20px;
+  display: flex;
+  align-items: center;
+  color: ${getPropsColor('secondary')};
+  border: 2px solid ${getPropsBackground('stroke')};
+  display: flex;
+  text-transform: none;
+
+  &:hover {
+    border: 2px solid ${getPropsBackground('primary')};
+    color: ${getPropsBackground('primary')};
+  }
+   & > * {
+    margin: 0 10px;
+  }
+`
+
+export default Button
