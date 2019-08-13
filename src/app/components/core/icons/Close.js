c6cfa12b9178942d7eb0fbc689bb884613a6933a
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-13 20:45:29 -0300

    FEA: add font awesome

---
+++ src/app/components/core/icons/Close.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+const Icon = (props) => (
+  <FontAwesomeIcon icon={faTimes} {...props} />
+)
+
+export default Icon
