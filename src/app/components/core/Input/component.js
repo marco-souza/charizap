5ae3556b888b0b384c332dad855f6ca786555b0c
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-17 16:44:38 -0300

    ENH: update Input component

--- src/app/components/core/Input/component.js
+++ src/app/components/core/Input/component.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Field } from 'formik'
+import { Field, ErrorMessage } from 'formik'
 
-import ErrorMessage from 'app/components/core/ErrorMessage'
+import ErrorMessageComponent from 'app/components/core/ErrorMessage'
 
-const Input = ({ className, label, errorMessage, ...restProps }) => (
+const Input = ({ className, label, ...restProps }) => (
   <div className={className}>
     {label && <label>{label}</label>}
 
     <Field component='input' type='text' {...restProps} />
-    <ErrorMessage>
-      {errorMessage}
-    </ErrorMessage>
+    <ErrorMessage component={ErrorMessageComponent} name={restProps.name} />
   </div>
 )
 
 Input.propTypes = {
   label: PropTypes.string,
-  errorMessage: PropTypes.string,
   className: PropTypes.string,
 }
 
