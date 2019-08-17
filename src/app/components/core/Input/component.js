3b409915a3229211eb37be00744851303ed07444
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-17 14:17:54 -0300

    ENH: add ErrorMessage to Input

--- src/app/components/core/Input/component.js
+++ src/app/components/core/Input/component.js
@@ -2,16 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Field } from 'formik'
 
-const Input = ({ className, label, ...restProps }) => (
+import ErrorMessage from 'app/components/core/ErrorMessage'
+
+const Input = ({ className, label, errorMessage, ...restProps }) => (
   <div className={className}>
     {label && <label>{label}</label>}
 
     <Field component='input' type='text' {...restProps} />
+    <ErrorMessage>
+      {errorMessage}
+    </ErrorMessage>
   </div>
 )
 
 Input.propTypes = {
   label: PropTypes.string,
+  errorMessage: PropTypes.string,
   className: PropTypes.string,
 }
 
