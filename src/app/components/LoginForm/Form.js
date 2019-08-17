0fcf14ff3a7343bda56cbe0e5059fdac320b9175
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-17 15:51:08 -0300

    ENH: use just one file to define Form

--- src/app/components/LoginForm/Form.js
+++ src/app/components/LoginForm/Form.js
@@ -1,65 +1,86 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { withFormik } from 'formik'
+import * as Yup from 'yup'
 
 import Button from 'app/components/core/Button'
 import Input from 'app/components/core/Input'
 import Link from 'app/components/core/Link'
-import styled from 'styled-components'
 
-const Form = ({
-  values,
-  errors,
-  touched,
-  handleChange,
-  handleBlur,
-  handleSubmit,
-  isSubmitting,
-  className,
-}) => (
-  <div className={className}>
-    <form onSubmit={handleSubmit}>
-      <Input
-        name='email'
-        label='Email Address'
-        placeholder='name@project.com'
-      />
+import {
+  propsToValues,
+  defaultFormikProps,
+  formIsValid,
+  REQUIRED,
+  EMAIL,
+  PASSWORD,
+} from 'app/helpers/forms'
+import { useWrappers } from 'app/helpers/redux'
+import useAuth from 'app/redux/auth'
 
-      <Input
-        label='Password'
-        placeholder='+5 characters'
-        type='password'
-        name='password'
-      />
+import { FormContainer } from './styled'
 
-      <Button type='submit' disable={isSubmitting}>
-        Login
-      </Button>
+const Form = (props) => {
+  const { errors, handleSubmit } = props
+  return (
+    <FormContainer>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name='email'
+          label='Email Address'
+          placeholder='name@project.com'
+          errorMessage={errors.email}
+        />
 
-      <Link href='#' small>
-        Forgot your password?
-      </Link>
-    </form>
-  </div>
-)
+        <Input
+          label='Password'
+          placeholder='+5 characters'
+          type='password'
+          name='password'
+          errorMessage={errors.password}
+        />
 
-Form.propTypes = {
-  values: PropTypes.object,
-  errors: PropTypes.object,
-  touched: PropTypes.object,
-  handleChange: PropTypes.func,
-  handleBlur: PropTypes.func,
-  handleSubmit: PropTypes.func,
-  isSubmitting: PropTypes.bool,
-  className: PropTypes.string,
+        <Button
+          type='submit'
+          disabled={!formIsValid(props)}
+        >
+          Login
+        </Button>
+
+        <Link href='#' small>
+          Forgot your password?
+        </Link>
+      </form>
+    </FormContainer>
+  )
 }
 
-export default styled(Form)`
-  & button {
-    width: 100%;
-    margin: 20px 0;
-  }
+Form.propTypes = {
+  ...defaultFormikProps,
+}
 
-  & form {
-    margin-top: 70px;
-  }
-`
+const validationSchema = Yup.object()
+  .shape({
+    email: Yup.string().email(EMAIL).required(REQUIRED),
+    password: Yup.string().min(6, PASSWORD).required(REQUIRED),
+  })
+
+const mapPropsToValues = propsToValues
+  .fromInitialState({
+    email: '',
+    password: '',
+  })
+
+const onSubmit = (values, { props }) => {
+  console.log(values, props)
+  props.login(values)
+}
+
+export default useWrappers(
+  withFormik({
+    enableReinitialize: true,
+    handleSubmit: onSubmit,
+    validationSchema,
+    mapPropsToValues,
+  }),
+  useAuth,
+)(Form)
