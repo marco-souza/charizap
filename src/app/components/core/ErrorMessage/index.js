7f758710abd1e5128c5021d1ae46279861fd759a
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-17 14:17:46 -0300

    FEA: add ErrorMessage component

---
+++ src/app/components/core/ErrorMessage/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+import { getColor } from 'app/helpers/theme'
+
+const ErrorMessage = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+  margin-top: 5px;
+  color: ${getColor('error')};
+  width: 100%;
+  display: ${({ children }) => children && children.length ? 'inline-block' : 'none'};
+`
+
+export default ErrorMessage
