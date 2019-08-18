454e6aaefcd9d533194b15e5db082b0739c6473f
Frattezi <pfrattezi@gmail.com>
2019-08-18 12:44:02 -0300

    FIX: fix signup grid errors and fonts

--- src/app/components/SignupForm/styled.js
+++ src/app/components/SignupForm/styled.js
@@ -4,11 +4,25 @@ import CoreHeader from 'app/components/core/Header'
 import { getFont, getColor } from 'app/helpers/theme'
 
 export const Container = styled.div`
-  margin: 0 27%;
+  margin: 0 22%;
   text-align: center;
   margin-top: 40px;
   font-family: ${getFont('robotoDefault')};
-  margin-right: 27%;
+
+  @media(max-width: 960px) {
+    margin: 0 10%;
+    margin-top: 20px;
+  }
+
+  & h1 {
+    margin-top: 7%;
+    margin-right: 25%;
+    max-width: 100%;
+    font-size: 36px;
+    @media (max-width: 960px) {
+      font-size: 18px;
+    }
+  }
 
   & button {
     width: 100%;
@@ -16,6 +30,7 @@ export const Container = styled.div`
   }
 
   & form {
+    margin-top: 30%;
     margin-top: 70px;
   }
 
