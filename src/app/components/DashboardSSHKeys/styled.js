003d28ea89018443308f77f1ea0923a1a65b1f7e
Frattezi <pfrattezi@gmail.com>
2019-08-18 20:08:41 -0300

    FIX: dashboard textArea and Input background color

--- src/app/components/DashboardSSHKeys/styled.js
+++ src/app/components/DashboardSSHKeys/styled.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
 import Grid from '@material-ui/core/Grid'
 
+import { getColor } from 'app/helpers/theme'
+
 export const GridItem = styled(Grid)`
   display: flex;
   & > div {
     flex: 1;
+  }
+
+  & textarea, input {
+    background: ${getColor('white')};
   }
 `
