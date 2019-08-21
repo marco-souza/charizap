d72f3ac593bef830a42ddc73a09a11a9fd733966
Frattezi <pfrattezi@gmail.com>
2019-08-21 17:51:09 -0300

    FIX: fix xLarge button size

--- src/app/components/core/Button/BaseButton.js
+++ src/app/components/core/Button/BaseButton.js
@@ -4,27 +4,22 @@ import { getFont, mediaQueryUpTo } from 'app/helpers/theme'
 import { removeBlueMark } from 'app/helpers/styles'
 
 const Button = styled.button`
-  font-size: 20px;
-  height: 64px;
+  font-size: 16px;
+  height: 42px;
+
   padding: 10px 32px;
-  border-radius: 32px 32px;
+  border-radius: 20px 20px;
   text-transform: uppercase;
   border: none;
   font-family: ${getFont('robotoDefault')};
   cursor: pointer;
-  min-width: 194px;
+  min-width: 168px;
 
   ${mediaQueryUpTo('xLarge', `min-width: 160px;`)}
 
   &:disabled {
     opacity: 0.8;
   }
-
-  ${mediaQueryUpTo('xLarge', `
-    font-size: 16px;
-    height: 42px;
-    border-radius: 20px 20px;
-    `)}
 
   ${mediaQueryUpTo('xSmall', `
     font-size: 14px;
