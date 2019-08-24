65118dd17c954db1ee7f08ea53ebb39b713edbbc
Frattezi <pfrattezi@gmail.com>
2019-08-24 18:39:46 -0300

    REF: simplify API calls

--- src/app/helpers/api.js
+++ src/app/helpers/api.js
@@ -1,50 +1,47 @@
 import { of } from 'rxjs'
 import { ajax } from 'rxjs/ajax'
-import { getCookie, COOKIE_KEY, COOKIE_REFRESH_KEY } from 'app/helpers/cookie'
+import { getCookie, COOKIE_KEY } from 'app/helpers/cookie'
 
 const BASE_URL = process.env.API_BASE_URL
-const defaultHeaders = {
+const defaultHeaders = () => ({
   Accept: 'application/json',
   'Content-Type': 'application/json',
-}
-
-export const handleRequestErrors = callback =>
-  ({ response }) => {
-    console.log(response)
-    // TODO: handle Unauthenticated requests
-    return of(callback(response))
-  }
+  Authorization: `Bearer ${getCookie(COOKIE_KEY)}`,
+})
 
 export const request = (url, options) =>
   ajax({
-    url,
+    url: `${BASE_URL}${url}`,
     ...options,
+    crossDomain: true,
+    headers: {
+      ...defaultHeaders(),
+      ...options.header,
+    },
   })
 
 export default {
-  signUp: body => request(`${BASE_URL}/users/signup`, {
+  signUp: body => request('/users/signup', {
     method: 'POST',
     body: body,
-    headers: defaultHeaders,
   }),
-  login: body => request(`${BASE_URL}/users/login`, {
+  login: body => request('/users/login', {
     method: 'POST',
     body: body,
-    headers: defaultHeaders,
   }),
-  refresh_key: () => request(`${BASE_URL}/users/refresh`, {
+  refresh: token => request('/users/refresh', {
     method: 'POST',
-    body: { refresh_token: `${getCookie(COOKIE_REFRESH_KEY)}` },
-    headers: defaultHeaders,
+    body: { refresh_token: token },
   }),
-  logout: options => request(`${BASE_URL}/users/logout`, {
-    method: 'GET',
-    headers: {
-      ...defaultHeaders,
-      Authorization: `Bearer ${getCookie(COOKIE_KEY)}`,
-    },
+  logout: options => request('/users/logout', {
     xhrFields: {
       withCredentials: true
     }
   }),
 }
+
+export const handleRequestErrors = callback =>
+  ({ response }) => {
+    console.log('TEST', response)
+    return of(callback(response))
+  }
