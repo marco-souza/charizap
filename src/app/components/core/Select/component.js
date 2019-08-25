5284da9463f614a97b335c43c13e82f5c27985de
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-25 17:10:31 -0300

    FEA: add new select component

--- src/app/components/core/Select/component.js
+++ src/app/components/core/Select/component.js
@@ -1,25 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ReactSelect from 'react-select'
 
-const Select = ({ className, value, handleChange, options, ...restProps }) => (
-  <ReactSelect
-    className={className}
-    value={value}
-    onChange={handleChange}
-    options={options}
-  />
+import { ErrorMessage } from 'formik'
+
+import ErrorMessageComponent from 'app/components/core/ErrorMessage'
+import { ReactSelect, Label, Container } from './styled'
+
+const Select = ({ label, hasError, className, options, ...restProps }) => (
+  <Container>
+    {label && <Label>{label}</Label>}
+
+    <ReactSelect
+      classNamePrefix='devopness'
+      className={className}
+      options={options}
+      {...restProps}
+    />
+    {hasError && <ErrorMessage component={ErrorMessageComponent} name={restProps.name} />}
+  </Container>
 )
 
 Select.propTypes = {
-  value: PropTypes.object.isRequired,
-  handleChange: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(PropTypes.shape({
     icon: PropTypes.node,
     value: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
   })).isRequired,
   className: PropTypes.string,
+  hasError: PropTypes.bool,
+  label: PropTypes.string,
 }
 
 export default Select
