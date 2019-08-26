a2c1c610f0b922003aacdc83ea4ea8edcdd310fe
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-26 17:35:12 -0300

    ENH: add styled to Signup Cover

---
+++ src/app/components/SignupCover/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import CoreCover from 'app/components/core/Cover'
+
+export const Cover = styled(CoreCover)`
+  & h5 {
+    padding: 40px 8%;
+  }
+`
+
+export const Container = styled.div`
+  /* overflow: hidden; */
+`
