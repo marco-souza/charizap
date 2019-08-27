d2ce5b565856be6e69c91f62d9e8db8fd811b0d9
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-27 13:49:26 -0300

    FEA: add allow iframe script

---
+++ allowIframe.js
@@ -0,0 +1,14 @@
+function allowIframe(details) {
+  return {
+    responseHeaders: details.responseHeaders
+      .filter(header =>
+        header.name.toLowerCase() == 'x-frame-options'
+      )
+  };
+}
+
+chrome.webRequest.onHeadersReceived.addListener(
+  allowIframe,
+  { urls: ["<all_urls>"] },
+  ["blocking", "responseHeaders"]
+);
