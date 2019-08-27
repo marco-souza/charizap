c3a47dcb9d80b44ab2f52156f1094ee831f3851b
Frattezi <pfrattezi@gmail.com>
2019-08-27 16:44:10 -0300

    REF: pass local state controll to Dialog

--- src/app/components/DialogAddProject/index.js
+++ src/app/components/DialogAddProject/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import MultiStepForm from 'app/components/core/MultiStepForm'
@@ -20,18 +20,56 @@ const steps = [
   WaitStep,
 ]
 
-const DialogContainer = ({ handleClose, modalIsVisible }) => (
-  <Dialog
-    title='Make it happen'
-    isOpen={modalIsVisible}
-    handleClose={handleClose}
-  >
-    <MultiStepForm
-      onSubmit={data => console.log('Loks', data)}
-      steps={steps}
-    />
-  </Dialog>
-)
+const DialogContainer = ({ handleClose, modalIsVisible }) => {
+  const [data, setData] = useState({})
+  const [step, setStep] = useState(0)
+  const [lastVisited, setLastVisited] = useState(0)
+  const hasNext = steps.length - 1 > step
+  const hasPrevious = step > 0
+  const Step = steps[step]
+
+  const nextStep = () => hasNext
+    ? (
+      setStep(step + 1),
+      setLastVisited(step + 1)
+    )
+    : console.log('Loks', data)
+
+  const previousStep = () => setStep(hasPrevious
+    ? step - 1
+    : step
+  )
+
+  const setStepOnCLick = (index, step) => setStep(index <= lastVisited
+    ? index
+    : step
+  )
+
+  const addData = newData => setData({
+    ...data,
+    ...newData,
+  })
+  return (
+    <Dialog
+      title='Make it happen'
+      isOpen={modalIsVisible}
+      handleClose={handleClose}
+    >
+      <MultiStepForm
+        steps={steps}
+        Step={Step}
+        step={step}
+        data={data}
+        hasNext={hasNext}
+        hasPrevious={hasPrevious}
+        addData={addData}
+        nextStep={nextStep}
+        previousStep={previousStep}
+        setStepOnCLick={setStepOnCLick}
+      />
+    </Dialog>
+  )
+}
 
 DialogContainer.propTypes = {
   modalIsVisible: PropTypes.bool.isRequired,
--- src/app/components/core/MultiStepForm/index.js
+++ src/app/components/core/MultiStepForm/index.js
@@ -1,65 +1,53 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import { DotStep, StepManager } from './styled'
 
-const MultiStepForm = ({ steps, onSubmit }) => {
-  const [data, setData] = useState({})
-  const [step, setStep] = useState(0)
-  const [lastVisited, setLastVisited] = useState(0)
-  const hasNext = steps.length - 1 > step
-  const hasPrevious = step > 0
-  const Step = steps[step]
+const MultiStepForm = ({
+  Step,
+  step,
+  steps,
+  data,
+  hasNext,
+  hasPrevious,
+  addData,
+  nextStep,
+  previousStep,
+  setStepOnCLick,
+}) => (
+  <div>
+    <Step
+      step={step}
+      data={data}
+      hasNext={hasNext}
+      hasPrevious={hasPrevious}
+      addData={addData}
+      nextStep={nextStep}
+      previousStep={previousStep}
+    />
 
-  const nextStep = () => hasNext
-    ? (
-      setStep(step + 1),
-      setLastVisited(step + 1)
-    )
-    : onSubmit(data)
-
-  const previousStep = () => setStep(hasPrevious
-    ? step - 1
-    : step
-  )
-
-  const setStepOnCLick = (index, step) => setStep(index <= lastVisited
-    ? index
-    : step
-  )
-
-  const addData = newData => setData({
-    ...data,
-    ...newData,
-  })
-
-  return (
-    <div>
-      <Step
-        step={step}
-        data={data}
-        hasNext={hasNext}
-        hasPrevious={hasPrevious}
-        addData={addData}
-        nextStep={nextStep}
-        previousStep={previousStep}
-      />
-
-      <StepManager>
-        {steps.map((key, index) => <DotStep
-          key={key}
-          className={index === step ? 'selected' : 'step'}
-          selected={index === step}
-          onClick={() => setStepOnCLick(index, step)}
-        />)}
-      </StepManager>
-    </div>
-  )
-}
+    <StepManager>
+      {steps.map((key, index) => <DotStep
+        key={key}
+        className={index === step ? 'selected' : 'step'}
+        selected={index === step}
+        onClick={() => setStepOnCLick(index, step)}
+      />)}
+    </StepManager>
+  </div>
+)
 
 MultiStepForm.propTypes = {
+  Step: PropTypes.func.isRequired,
+  step: PropTypes.number.isRequired,
   steps: PropTypes.array.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+  hasNext: PropTypes.isRequired,
+  hasPrevious: PropTypes.isRequired,
+  addData: PropTypes.func.isRequired,
+  nextStep: PropTypes.func.isRequired,
+  previousStep: PropTypes.func.isRequired,
+  setStepOnCLick: PropTypes.func.isRequired,
 }
 
 export default MultiStepForm
